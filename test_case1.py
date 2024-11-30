8975f7870ae6cab248306717d1e4467790409077
Apipon "Pip" Siripaisan <59779016+pipsta9@users.noreply.github.com>
2024-11-29 20:25:09 -0800

    Add files via upload

---
+++ test_case1.py
@@ -0,0 +1,41 @@
+from odoo.tests import common, new_test_user
+
+
+class TestFleet(common.TransactionCase):
+
+    def setUp(self):
+        super(TestFleet, self).setUp()
+
+        brand = self.env["fleet.vehicle.model.brand"].create({
+            "name": "Toyota",
+        })
+
+        self.brand_name = brand.name
+
+        self.vehicle = self.env["fleet.vehicle.model"].create({
+            "brand_id": brand.id,
+            "name": "Prius",
+            "color": "Red",
+            "horsepower" : 200,
+            "seats" : 5
+        })
+
+    def test_vehicle_model(self):
+
+        print(self.vehicle.color)
+        print(self.vehicle.name)
+
+        #msg print only if returns False
+        self.assertIsNotNone(self.vehicle.brand_id, "Brand name is missing")
+        self.assertEqual(self.brand_name, "Toyota", "Brand name does not match")
+        self.assertEqual(self.vehicle.name, "Prius", "Model name does not match")
+        self.assertEqual(self.vehicle.color, "Red", "Model color does not match")
+        self.assertEqual(self.vehicle.horsepower, 200, "Model horsepower does not match")
+        self.assertEqual(self.vehicle.seats, 5, "Model seats does not match")
+
+    def test_vehicle_update(self):
+
+        self.vehicle.update({"color":  "Blue"})
+        print(self.vehicle.color)
+
+        self.assertEqual(self.vehicle.color, "Blue", "Model color does not match")
