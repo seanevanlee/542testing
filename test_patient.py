daf94389c932bf9653f329b8f5d5d1a527495295
dorisleong <doris.may.leong@gmail.com>
2024-12-03 00:29:44 -0800

    Added more test cases
    5 unit test cases

--- test_patient.py
+++ test_patient.py
@@ -14,9 +14,26 @@ class TestPatient(TransactionCase):
         cls.patient = cls.env['hospital.patient'].create({
             'name': 'John Test',
             'age': 18,
-            'reference': cls.id,
             'gender': 'male',
             'note': 'test case patient'
+        })
+        
+        cls.doctor = cls.env['hospital.doctor'].create({
+            'doctor_name': 'Doc Brown',
+            'age': 85,
+            'gender': 'other'
+        })
+
+        # create 3 appointments for John Test patient with Doc Brown
+        cls.appointment_1 = cls.env['hospital.appointment'].create({
+            'patient_id': cls.patient.id,
+            'doctor_id': cls.doctor.id,
+            'state': 'done'
+        })
+
+        cls.appointment_2 = cls.env['hospital.appointment'].create({
+            'patient_id': cls.patient.id,
+            'doctor_id': cls.doctor.id
         })
         
     def test_create_patient(self):
@@ -24,6 +41,7 @@ class TestPatient(TransactionCase):
         self.assertTrue(self.patient, "Patient should be created successfully")
         self.assertEqual(self.patient.name, 'John Test', "Name field value is incorrect")
         self.assertEqual(self.patient.gender, 'male', "Gender field value is incorrect")
+        self.assertEqual(self.patient.age, 18, "Age is incorrect")
 
     def test_age_error(self):
         """Test that error is thrown when age is 0"""
@@ -31,4 +49,20 @@ class TestPatient(TransactionCase):
             self.patient.age = 0
     
     def test_appointment_count(self):
-        """TODO:Test that amount of appointments for patient is 3 with compute function"""
+        """Test that amount of appointments for patient is 2 with compute function"""
+        self.assertEqual(self.patient.appointment_count, 2, "Appointment count should be 3")
+
+    def test_unlink_error(self):
+        """Test that an error throws if appointment is attempted to be deleted while in done state"""
+        with self.assertRaises(ValidationError):
+            self.appointment_1.unlink()
+
+    def test_patient_names(self):
+        """Test that an error is thrown if patient with a same name is added"""
+        with self.assertRaises(ValidationError):
+            patient_duplicate = self.env['hospital.patient'].create({
+                'name': 'John Test',
+                'age': 8,
+                'gender': 'female',
+            })
+
