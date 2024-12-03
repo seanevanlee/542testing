dbd08eb7b94958691220f349de259be844e36d9a
Apipon "Pip" Siripaisan <59779016+pipsta9@users.noreply.github.com>
2024-12-02 21:45:12 -0800

    Update test_case1.py

--- test_case1.py
+++ test_case1.py
@@ -1,4 +1,7 @@
+from xlwt.ExcelFormulaLexer import false_pattern
+
 from odoo.tests import common
+from datetime import date, datetime
 
 
 class TestFleet(common.TransactionCase):
@@ -6,8 +9,19 @@ class TestFleet(common.TransactionCase):
     def setUp(self):
         super(TestFleet, self).setUp()
 
+        self.driver = self.env['res.partner'].create({
+            'name': 'Test Driver',
+            'email': 'driver@example.com',
+        })
+
         brand = self.env["fleet.vehicle.model.brand"].create({
             "name": "Toyota",
+        })
+
+        vehicle = self.env['fleet.vehicle'].create({
+            "id": "12345",
+            "model_id": "1",
+            "driver_id": self.driver.id
         })
 
         self.brand_name = brand.name
@@ -16,16 +30,28 @@ class TestFleet(common.TransactionCase):
             "brand_id": brand.id,
             "name": "Prius",
             "color": "Red",
-            "horsepower" : 200,
-            "seats" : 5
+            "horsepower": 200,
+            "seats": 5,
+        })
+
+        self.contract_date = self.env["fleet.vehicle.log.contract"].create({
+            "vehicle_id": vehicle.id,
+            "start_date": date(2022, 4, 30),
+            "expiration_date": date(2024, 8, 30),
+        })
+
+        self.odometer = self.env['fleet.vehicle.odometer'].create({
+            'vehicle_id': vehicle.id,
+            'value': 1000,
+        })
+
+        self.odometer2 = self.env['fleet.vehicle.odometer'].create({
+            'vehicle_id': vehicle.id,
+            'value': 980,
         })
 
     def test_vehicle_model(self):
-
-        print(self.vehicle.color)
-        print(self.vehicle.name)
-
-        #msg print only if returns False
+        # msg print only if returns False
         self.assertIsNotNone(self.vehicle.brand_id, "Brand name is missing")
         self.assertEqual(self.brand_name, "Toyota", "Brand name does not match")
         self.assertEqual(self.vehicle.name, "Prius", "Model name does not match")
@@ -34,12 +60,35 @@ class TestFleet(common.TransactionCase):
         self.assertEqual(self.vehicle.seats, 5, "Model seats does not match")
 
     def test_vehicle_update(self):
-
-        self.vehicle.update({"color":  "Blue"})
-        print(self.vehicle.color)
+        self.vehicle.write({"color": "Blue"})
 
         self.assertEqual(self.vehicle.color, "Blue", "Vehicle color did not update")
 
-    def tearDown(self):
-        """ This method is run after each test """
-        super(TestFleet, self).tearDown()
+    def test_compute_contract_reminder(self):
+        self.overdue = False
+
+        if (self.contract_date.expiration_date - date.today()).days < 0:
+            self.overdue = True
+
+        self.assertTrue(self.overdue, "Contract due date did not calculate correctly")
+
+    # test odometer second odometer value entered cannot be less than initial
+    def test_odometer_reading(self):
+        self.odometer = self.odometer.value
+        self.odometer2 = self.odometer2.value
+
+        self.assertEqual(self.odometer, 1000, "Initial odometer value did not get created correctly")
+        self.assertEqual(self.odometer2, 980, "Second odometer value did not get created correctly")
+        self.assertGreater(self.odometer2, self.odometer, "Second odometer value entered invalid")
+
+    def test_assign_driver(self):
+        # Check if the driver is correctly assigned
+        self.assertEqual(
+            self.env['fleet.vehicle'].driver_id,
+            self.driver,
+            "The driver should be correctly assigned to the vehicle."
+        )
+
+
+def tearDown(self):
+    super(TestFleet, self).tearDown()
